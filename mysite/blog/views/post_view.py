750173e289ee709788f5c572e793680117918b7c
Daniel Prazeres <contato@danielprazeres.com>
2025-01-23 22:58:38 -0300

    Criados templates
    Criados index.html, base.html, post_detail.html e sidebar.html. Também foi feito o update dos posts de status 0 (draft) para 1 (published)

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.views import generic
 
+from blog.models.post import Post
+
 
 class PostView(generic.ListView):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, World!')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+# class PostSidebar(generic.ListView):
+#     queryset = Post.objects.filter(status=1).order_by('-created_on')[:5]
+#     template_name = 'sidebar.html'
